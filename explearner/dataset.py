63d5ff20497cda8376906e38837e6e5ac451b53b
mutandon <davide.mottin@gmail.com>
2020-10-14 10:59:24 +0200

    Added Bank dataset and decision tree

--- explearner/dataset.py
+++ explearner/dataset.py
@@ -1,9 +1,18 @@
+import os
+import requests
+
+from tkinter.ttk import Label
+
 import numpy as np
+import pandas as pd
 
 from abc import ABC, abstractmethod
+
+from sklearn.metrics import jaccard_score
 from sklearn.utils import check_random_state
 from sklearn.model_selection import GridSearchCV
 from sklearn.gaussian_process.kernels import RBF, DotProduct
+from sklearn import tree
 from scipy.stats import norm
 from scipy.spatial.distance import cosine as cosine_dist
 from itertools import product, combinations
@@ -36,6 +45,7 @@ class Dataset(ABC):
     rng : int or RandomState or None
         The RNG.
     """
+
     def __init__(self, X, Z, y, kx, kz, ky, arms, combiner='prod', rng=None):
         assert X.ndim == 2
         assert Z.ndim == 2
@@ -76,8 +86,17 @@ class Dataset(ABC):
         return clf.best_estimator_
 
 
-class LineDataset(Dataset):
+class NormNormRewardMixin:
+    """Implements a simple reward function for scalar explanations."""
+    def reward(self, i, zhat, yhat, noise=0):
+        x, z, y = self.X[i,0], self.Z[i,0], self.y[i]
+        reward_z = norm(loc=z, scale=0.1).pdf(zhat[0])
+        reward_y = norm(loc=y, scale=0.1).pdf(yhat)
+        return reward_z * reward_y + self.rng.normal(0, noise)
+
+class LineDataset(NormNormRewardMixin, Dataset):
     """Toy 1-D dataset."""
+
     def __init__(self, **kwargs):
         X = np.linspace(-1, 1, num=51).reshape(-1, 1)
         Z = np.ones((X.shape[0], 1))
@@ -93,19 +112,13 @@ class LineDataset(Dataset):
 
         super().__init__(X, Z, y, kx, kz, ky, arms, **kwargs)
 
-    def reward(self, i, zhat, yhat, noise=0):
-        x, z, y = self.X[i,0], self.Z[i,0], self.y[i]
-        reward_z = norm(loc=z, scale=0.1).pdf(zhat[0])
-        reward_y = norm(loc=y, scale=0.1).pdf(yhat)
-        #print(x, 'zs', z, zhat, reward_z, 'ys', y, yhat, reward_y)
-        return reward_z * reward_y + self.rng.normal(0, noise)
 
-
-class SineDataset(Dataset):
+class SineDataset(NormNormRewardMixin, Dataset):
     """Toy 1-D dataset."""
+
     def __init__(self, **kwargs):
         X = np.linspace(0, 30, num=51).reshape(-1, 1)
-        Z = -np.cos(X * 0.5) # XXX really?
+        Z = -np.cos(X * 0.5)  # XXX really?
         y = np.sin(X * 0.5).ravel()
 
         # TODO cosine kernel for z
@@ -119,24 +132,19 @@ class SineDataset(Dataset):
 
         super().__init__(X, Z, y, kx, kz, ky, arms, **kwargs)
 
-    def reward(self, i, zhat, yhat, noise=0):
-        x, z, y = self.X[i,0], self.Z[i,0], self.y[i]
-        reward_z = norm(loc=z, scale=1).pdf(zhat[0])
-        reward_y = norm(loc=y, scale=1).pdf(yhat)
-        return reward_z * reward_y + self.rng.normal(0, noise)
-
 
 _COLORS = [
-    (255,   0,   0), # r
-    (0,   255,   0), # g
-    (0,   128, 255), # b
-    (128,   0, 255), # v
+    (255, 0, 0),  # r
+    (0, 255, 0),  # g
+    (0, 128, 255),  # b
+    (128, 0, 255),  # v
 ]
 _COLOR_TO_INDEX = {tuple(color): i for i, color in enumerate(_COLORS)}
 
 
 class ColorsDataset(Dataset):
     """Toy dataset used by RRR and CAIPI."""
+
     def __init__(self, **kwargs):
         self.rule = kwargs.pop('rule')
 
@@ -157,9 +165,9 @@ class ColorsDataset(Dataset):
             Z = np.array([self._explain(x) for x in X])
             dump(path, (X, Z, y))
 
-        #kx = gpflow.kernels.RBF(active_dims=list(range(0, 25)))
-        #kz = gpflow.kernels.RBF(active_dims=list(range(25, 50)))
-        #ky = gpflow.kernels.Linear(active_dims=[50])
+        # kx = gpflow.kernels.RBF(active_dims=list(range(0, 25)))
+        # kz = gpflow.kernels.RBF(active_dims=list(range(25, 50)))
+        # ky = gpflow.kernels.Linear(active_dims=[50])
 
         arms = self._enumerate_arms()
         super().__init__(X, Z, y, [0, 0, 0], arms, **kwargs)
@@ -167,17 +175,17 @@ class ColorsDataset(Dataset):
     @staticmethod
     def _img_to_x(img):
         img = img.reshape((5, 5, 3))
-        x = [_COLOR_TO_INDEX[tuple(img[r,c])]
+        x = [_COLOR_TO_INDEX[tuple(img[r, c])]
              for r, c in product(range(5), repeat=2)]
         return np.array(x, dtype=np.float32)
 
     @staticmethod
     def _rule0(x):
-        return int(x[0,0] == x[0,4] and x[0,0] == x[4,0] and x[0,0] == x[4,4])
+        return int(x[0, 0] == x[0, 4] and x[0, 0] == x[4, 0] and x[0, 0] == x[4, 4])
 
     @staticmethod
     def _rule1(x):
-        return int(x[0,1] != x[0,2] and x[0,1] != x[0,3] and x[0,2] != x[0,3])
+        return int(x[0, 1] != x[0, 2] and x[0, 1] != x[0, 3] and x[0, 2] != x[0, 3])
 
     def _classify(self, x):
         return {0: self._rule0, 1: self._rule1}[self.rule](x.reshape((5, 5)))
@@ -189,7 +197,7 @@ class ColorsDataset(Dataset):
         }[self.rule]
 
         x = x.reshape((5, 5))
-        counts = np.bincount([x[r,c] for r, c in coords])
+        counts = np.bincount([x[r, c] for r, c in coords])
         max_count, max_value = np.max(counts), np.argmax(counts)
 
         z = np.zeros_like(x, dtype=np.float32)
@@ -218,7 +226,7 @@ class ColorsDataset(Dataset):
                     z = np.zeros((5, 5), dtype=np.int8)
                     for (r, c), s in zip(coords, signs):
                         z[r, c] = 2 * s - 1
-                    arms.append((z.reshape(5*5), label))
+                    arms.append((z.reshape(5 * 5), label))
 
         return arms
 
@@ -252,3 +260,86 @@ class AdultDataset(Dataset):
 
     def reward(self, i, zhat, yhat, noise=0):
         raise NotImplementedError()
+
+
+class BanknoteAuth(Dataset):
+    def __init__(self, **kwargs):
+        urls = ["https://archive.ics.uci.edu/ml/machine-learning-databases/00267/data_banknote_authentication.txt"]
+        self.load_dataset('data', urls)
+
+        # total 1372 instances, 610 1s = fake, 762 0s = real
+        columns = ["variance", "skew", "curtosis", "entropy", "class"]
+        dataset = pd.read_csv("data/data_banknote_authentication.txt", names=columns)
+
+        # target values: 1 is fake, 0 is real
+        y = dataset['class'].to_numpy()
+        # creating the feature vector
+        X = dataset.drop('class', axis=1).to_numpy()
+
+        clf = tree.DecisionTreeClassifier()
+        clf = clf.fit(X, y)
+        self.tree = clf.tree_
+        # Extremely sparse explanations
+        Z = clf.decision_path(X).toarray()
+
+        # Avoid sparse features - Maybe in the future
+        # decision_path = clf.decision_path(X).toarray()
+        # for i in np.arange(len(decision_path.indptr) - 1):
+        #     decision_path.indices[decision_path.indptr[i]:decision_path.indptr[i+1]]
+        # Z = np.array(Z)
+
+        # Kernels
+        kx = RBF(length_scale=0.1, length_scale_bounds=(0.1, 0.1))
+        kz = DotProduct(sigma_0=1, sigma_0_bounds=(1, 1))  # Explanations are sparse
+        ky = DotProduct(sigma_0=1, sigma_0_bounds=(1, 1))
+
+        # Extract all possible root-to-leaf explanations
+        arms_z = self.root_to_leaf_paths(0)
+        arms_y = np.array([0, 1])
+        arms = list(product(arms_z, arms_y))
+        super().__init__(X, Z, y, kx, kz, ky, arms, **kwargs)
+
+    def reward(self, i, zhat, yhat, noise=0):
+        z, y = self.Z[i], self.y[i]
+
+        sign = 1 if y == yhat else -1
+        return sign * (jaccard_score(z, zhat)) + self.rng.normal(0, noise)
+
+    @staticmethod
+    def load_dataset(path, urls):
+        """
+        Download the content for a list of URLs and save them to a folder
+        :param path: The path to the location where the data will be saved
+        :param urls: The list of URLs from which the content will be downloaded and saved
+        """
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+        for url in urls:
+            data = requests.get(url).content
+            filename = os.path.join(path, os.path.basename(url))
+            with open(filename, "wb") as file:
+                file.write(data)
+
+    def root_to_leaf_paths(self, node_id):
+        """
+        Finds all root-to-leaf paths in a decision tree.
+        Returns: All paths from root-to-leaf
+        """
+        # The node is a leaf
+        if self.tree.children_left[node_id] == self.tree.children_right[node_id]:
+            path = np.zeros(self.tree.node_count)
+            path[node_id] = 1
+            return [path]
+        # Recursively scan left and right children
+        else:
+            paths = []
+            left_paths = self.root_to_leaf_paths(self.tree.children_left[node_id])
+            for path in left_paths:
+                path[node_id] = 1
+                paths.append(path)
+            right_paths = self.root_to_leaf_paths(self.tree.children_right[node_id])
+            for path in right_paths:
+                path[node_id] = 1
+                paths.append(path)
+            return paths
