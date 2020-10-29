50f1dbae18ce77d78c7572c437dfacbbf7af4296
Stefano Teso <stefano.teso@gmail.com>
2020-10-29 08:05:53 +0100

    cgpucb: speed up inference and clean up.

--- explearner/cgpucb.py
+++ explearner/cgpucb.py
@@ -5,11 +5,8 @@ from sklearn.gaussian_process import GaussianProcessRegressor as GPR
 
 class CGPUCB(GPR):
     def __init__(self, **kwargs):
-        strategy = kwargs.pop('strategy', 'random')
-        self.select_arm = {
-            'random': self._select_arm_at_random,
-            'ucb': self._select_arm_by_ucb,
-        }[strategy]
+        self.strategy = kwargs.pop('strategy', 'random')
+        self.batch_size = kwargs.pop('batch_size', 1024)
         super().__init__(**kwargs)
 
     def fit(self, X, Z, y, f):
@@ -22,28 +19,56 @@ class CGPUCB(GPR):
         D = np.concatenate((X, Z, y.reshape(-1, 1)), axis=1)
         return super().predict(D, **kwargs)
 
+    def _argmax_arm(self, dataset, x, f, requires_std=False):
+        """Find the maximum value of f over the arms.  Uses batches to
+        reduce the number of calls to self.predict()."""
+        best_value, best_index = -np.inf, None
+        for offs in range(0, len(dataset.arms), self.batch_size):
+
+            batch = np.array(dataset.arms[offs:offs + self.batch_size],
+                             dtype=object)
+            X = np.vstack((x,) * len(batch))
+            Z = np.vstack(tuple(batch[:, 0]))
+            y = batch[:, 1]
+
+            if requires_std:
+                mean, std = self.predict(X, Z, y, return_std=True)
+                values = f(mean, std)
+            else:
+                mean = self.predict(X, Z, y, return_std=False)
+                values = f(mean)
+
+            index = np.argmax(values)
+            if values[index] > best_value:
+                best_value = values[index]
+                best_index = index + offs
+
+        return best_index
+
     def predict_arm(self, dataset, x):
         """Returns the best arm for context x."""
-        x = np.array([x])
+        j = self._argmax_arm(dataset,
+                             np.array([x]),
+                             lambda mean: mean,
+                             requires_std=False)
+        return dataset.arms[j]
 
-        def mean_reward(arm):
-            z, y = arm
-            return self.predict(x, np.array([z]), np.array([[y]]))[0]
-
-        return max(dataset.arms, key=mean_reward)
-
-    def _select_arm_by_ucb(self, dataset, x, beta=1.0):
-        """Returns the arm that maximizes the UCB of x."""
-        x = np.array([x])
-
-        def ucb(arm):
-            z, y = arm
-            mean, std = self.predict(x, np.array([z]), np.array([[y]]),
-                                     return_std=True)
-            return mean[0] + np.sqrt(beta) * std[0]
-
-        return max(dataset.arms, key=ucb)
-
-    def _select_arm_at_random(self, dataset, x, beta=1.0):
+    def _select_arm_at_random(self, dataset, x, beta):
         """Returns a random arm."""
         return dataset.arms[self.random_state.choice(len(dataset.arms))]
+
+    def _select_arm_by_ucb(self, dataset, x, beta):
+        """Returns the arm that maximizes the UCB of x."""
+        j = self._argmax_arm(dataset,
+                             np.array([x]),
+                             lambda mean, std: mean + np.sqrt(beta) * std,
+                             requires_std=True)
+        return dataset.arms[j]
+
+    def select_arm(self, dataset, x, beta=1.0):
+        if self.strategy == 'random':
+            return self._select_arm_at_random(dataset, x, beta)
+        elif self.strategy == 'ucb':
+            return self._select_arm_by_ucb(dataset, x, beta)
+        else:
+            raise ValueError('strategy must be "random" or "ucb"')
